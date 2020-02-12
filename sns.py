8fa1f5755e53cb0cf7433e97deb6774b2641c7a0
zwivhuyamashau <zwivhuya@gmail.com>
2020-02-12 21:17:44 +0200

    Add files via upload

---
+++ sns.py
@@ -0,0 +1,22 @@
+"code that uses AWS SNS to send a lot of messages to her aunt on her birthday so that she leaves me alone"
+
+import boto3
+import time
+
+class SendToAunt():
+    def __init__(self,filename,number):
+        self.client=client = boto3.client('sns')
+        self.filename=filename
+        self.number=number
+    
+    def openFileAndSend(self):
+        file = open(self.filename, "r",encoding="utf8")
+        listOfLines=file.readlines()
+        
+        for line in listOfLines:
+            response = self.client.publish(PhoneNumber=self.number,Message=line)
+            print(line)
+            time.sleep(1)
+
+messageObject=SendToAunt("sns.txt","+27793044025")
+messageObject.openFileAndSend()
